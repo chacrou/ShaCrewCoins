53adea3c7eb38535777ed28e97ed4c10685f333f
chacrou <127198409+chacrou@users.noreply.github.com>
2023-04-01 18:21:17 +0200

    Add files via upload

--- converter.py
+++ converter.py
@@ -5,7 +5,6 @@ import Fonctions as f
 session = scratch3.Session(".eJxVj0trwzAQhP-Lzq1rybIl59bkUEqhAVNoexKrh235Ibm2TCCl_70S5JLTLt_sDLO_aN_M6mA26IBOPajV7-gBCdhDL5IkrI5KzUlV8CKPUjBbUN6PNjkufh2NvjdIUKNxyZWYccEqCNa77CZsWWOW6QaPt-OY6-MSTUQqXBNdQl7XlCnNpWaKYllJDoYW5HAtxfBSkJ_j-ePU5Mp2b8P5Sy-i-XyNMZPvrHu0S0rCJMOYZiSOmqaSE7huhy41b9cI9BCBF8HO5updws-zWWO1p3dzEd_xufvXetj6eFTyClRJgQGj0NaYt5pw0IrlhWmlxpppzGSVo79_3q5xlw:1pSYz9:LaYT1p_uAAaNgEydXPeX4MWjRCY", username="Chacrou") #replace with your session_id and username
 conn = session.connect_cloud("829566680") #replace with your project id
 client = scratch3.CloudRequests(conn)
-print('ShaCrewCoins')
 
 #Just to check the server status
 @client.request
@@ -14,24 +13,37 @@ def avaible():
 
 
 @client.request
-def get_scc(argument1):
-    my_balance = f.read_json(argument1)
-    my_balance = my_balance[0]
-    my_balance = my_balance["balance"]
-    return my_balance
+def get_balance(argument1, argument2):
+    if  argument2 == 'scc':
+        my_balance = f.read_json(argument1)
+        my_balance = my_balance[0]
+        my_balance = my_balance["balance"]
+        return my_balance
 @client.request
 
-def set_balance_scc(argument1, argument2):
-    my_file = f.read_json(argument1)
-    balance = my_file[0]
-    balance = balance["balance"]
-    save = my_file[1]
-    somme = int(argument2)
-    if balance < somme:
-        return 'no'
-    balance = balance + somme
-    my_file = [{"balance":balance}, save]
-    f.write_json(argument1, my_file, "w")
-    return ('Good')
+def set_balance(argument1, argument2):
+    n = 0
+    who = ''
+    to = ''
+    for chars in argument1:
+        if n == 1:
+            to = to + chars
+        if n == 0:
+            if chars != ';':
+                who = who + chars
+            else:
+                n = 1
+    if to == 'scc':    
+        my_file = f.read_json(who)
+        balance = my_file[0]
+        balance = balance["balance"]
+        save = my_file[1]
+        somme = int(argument2)
+        if balance < somme:
+            return 'no'
+        balance = balance + somme
+        my_file = [{"balance":balance}, save]
+        f.write_json(who, my_file, "w")
+        return ('Good')
 
 client.run()
